577eac3807159055abd1d61442438ae170b7c21d
Lishinho <lishinho95@gmail.com>
2019-04-04 12:09:14 +0900

    Add files via upload

--- externalSortLZB/src/externalSort/test.java
+++ externalSortLZB/src/externalSort/test.java
@@ -6,11 +6,11 @@ import java.io.File;
 public class test {
     public static void main(String[] args) throws Exception {
         long start=System.currentTimeMillis();
-        File inputFile=new File("E:\\website\\myInputFile.txt");
-        File outputFile=new File("E:\\website\\outputFile.txt");
-        File tempFile=new File("E:\\website\\tempFile");
-        if (outputFile.exists())
-            outputFile.delete();
+        File inputFile=new File("C:\\newFile");
+        File outputFile=new File("C:\\outputFile");
+        File tempFile=new File("C:\\tempFile");
+//        if (outputFile.exists())
+//            outputFile.delete();
         main.test(inputFile,outputFile,tempFile);
         long end=System.currentTimeMillis();
         System.out.println(end-start);
